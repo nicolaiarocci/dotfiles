77850359b16c5ebb49e77ecb61391bcd69926108
Nicola Iarocci <nicola@nicolaiarocci.com>
2013-05-18 12:35:06 +0200

    Not using freshshell anymore; just symlinks

--- install.sh
+++ install.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
-echo "Cloning fresh..."
-git clone https://github.com/freshshell/fresh ~/.fresh/source/freshshell/fresh
-echo "Setting up fresh..."
-ls -s  ~/.dotfiles/fresh/freshrc ~/.freshrc
-~/.fresh/source/freshshell/fresh/bin/fresh
+
+#bash
+echo "Setting up bash..."
+ln -s ~/.dotfiles/bash/bash_profile ~/.bash_profile
+
+# git
+echo "Setting up git..."
+ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/git/gitignore_global ~/.gitignore_global
+
+#iterm2
+echo "Setting up iTerm2..."
+ln -s ~/.dotfiles/iterm2/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
+
+#vim
+echo "Setting up vim..."
+ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
 echo "Cloning vundle for Vim..."
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+#git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 echo "Install Vim plugins..."
 vim +BundleInstall +qall
